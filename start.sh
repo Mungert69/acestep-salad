854d5ca104a59619b4171539c1640536dfe82599
Mungert69 <contact@mahadeva.co.uk>
2026-07-30 23:26:14 +0000

    fixed startup issue dont try and run python3 server first

--- start.sh
+++ start.sh
@@ -45,7 +45,7 @@ download_model() {
 
 download_model "Qwen3-Embedding-0.6B-Q8_0.gguf"
 download_model "acestep-5Hz-lm-4B-Q5_K_M.gguf"
-download_model "acestep-v15-sft-Q6_K.gguf"
+download_model "acestep-v15-sft-Q4_K_M.gguf"
 download_model "vae-BF16.gguf"
 
 
